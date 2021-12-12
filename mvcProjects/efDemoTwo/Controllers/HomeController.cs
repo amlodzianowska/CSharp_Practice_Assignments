f44f84892d34249de0dbbed30fc3b1e3dc71dd1a
amlodzianowska <an.mlodzianowska@gmail.com>
2021-12-12 09:56:17 -0600

    Full demo

--- mvcProjects/efDemoTwo/Controllers/HomeController.cs
+++ mvcProjects/efDemoTwo/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using efDemoTwo.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace efDemoTwo.Controllers
 {
     public class HomeController : Controller
@@ -23,13 +24,61 @@ namespace efDemoTwo.Controllers
 
         public IActionResult Index()
         {
+            //                       DB   Table  .Actions
+            ViewBag.allAnimals = _context.Animals.ToList();
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost("addAnimal")]
+        public IActionResult addAnimal(Animal newAnimal)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(newAnimal);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }else{
+                return View("Index");
+            }
         }
+
+        [HttpGet("animal/{anId}")]
+        public IActionResult oneAnimal(int anId)
+        {
+            Animal one = _context.Animals.FirstOrDefault(a => a.AnimalId == anId);
+            return View(one);
+        }
+
+        [HttpGet("delete/{anId}")]
+        public IActionResult deleteAnimal(int anId)
+        {
+            Animal one = _context.Animals.FirstOrDefault(a => a.AnimalId == anId);
+            _context.Remove(one);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("edit/{anId}")]
+        public IActionResult Edit(int anId)
+        {
+            Animal oneAnimal = _context.Animals.FirstOrDefault(a => a.AnimalId == anId);
+            return View(oneAnimal);
+        }
+
+        [HttpPost("updateAnimal/{anId}")]
+        public IActionResult Update(int anId, Animal edited)
+        {
+            Animal original = _context.Animals.FirstOrDefault(a => a.AnimalId == anId);
+            original.Species = edited.Species;
+            original.numLegs = edited.numLegs;
+            original.isMammal = edited.isMammal;
+            original.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- mvcProjects/efDemoTwo/Models/Animal.cs
+++ mvcProjects/efDemoTwo/Models/Animal.cs
@@ -6,7 +6,9 @@ namespace efDemoTwo.Models
     {
         [Key]
         public int AnimalId {get;set;}
+        [Required]
         public string Species {get;set;}
+        [Range(0,100)]
         public int numLegs {get;set;}
         public bool isMammal {get;set;}
         public DateTime CreatedAt {get;set;} = DateTime.Now;
